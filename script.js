b2a614fbd9d325d44cc700ee14e76a29a114c71b
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-24 12:12:45 +0800

    Manage players inside controller

--- script.js
+++ script.js
@@ -45,7 +45,15 @@ function GameController(
             marker: 'y'
         }
     ];
+
+    let activePlayer = players[0];
     
+    const switchPlayerTurn = () =>{
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
 
 }
 
