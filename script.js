a4cc6162051f7d141e482112c99863caecd9619f
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-24 10:50:50 +0800

    Create 3x3 GameBoard()

---
+++ script.js
@@ -0,0 +1,16 @@
+function GameBoard(){
+    const row = 3;
+    const column =3;
+    const board = []; 
+
+    for(let i = 0; i < row; i++){
+        board[i] = [];
+        for(let j = 0; j < column; j++){
+            board[i].push(j);
+        }
+    }
+
+    return board;
+}
+
+console.log(GameBoard());
